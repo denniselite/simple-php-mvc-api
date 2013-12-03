9ab6800b10371b6e7ef52029e38c46d66c09f0ab
Denis Podlesnykh <denniselite@live.com>
2013-12-03 14:35:36 +0400

    base

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -2,8 +2,8 @@
 
 include_once Q_PATH.'/application/core/controller.php';
 include_once Q_PATH.'/application/core/model.php';
-include_once Q_PATH.'/application/core/request.php';
-include_once Q_PATH.'/application/core/route.php';
+include_once Q_PATH.'/application/system/request.php';
+include_once Q_PATH.'/application/system/route.php';
 include_once Q_PATH.'/application/core/view.php';
 
 Route::Start();
