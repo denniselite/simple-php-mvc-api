ec0db9b9df975bc037e0d0ad438b6303f059e9b8
Denis Podlesnykh <denniselite@live.com>
2013-12-03 20:11:30 +0400

    Начал контроллер users

---
+++ application/models/Model_users.php
@@ -0,0 +1,25 @@
+<?php
+
+Class Model_users extends Model{
+    
+    function __construct() {
+        
+    }
+    
+    public function create_user($uid,$e_mail,$pass) {
+        
+    }
+    
+    public function delete_user() {
+        
+    }
+    
+    public function get_profile() {
+        
+    }
+    
+    public function edit_profile() {
+        
+    }
+    
+}
--- application/views/template.php
+++ application/views/template.php
@@ -1,10 +1,1 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <?php
-            include Q_PATH.'/application/views/View_'.$view.'.php';
-        ?>
-    </body>
-</html>
+<?php include Q_PATH.'/application/views/View_'.$view.'.php'; ?>
